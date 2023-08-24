76c1f9fd411f8d1723d5c65785431142707432ec
HP <kajaldewade9700@gmail.com>
2023-08-24 15:08:38 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "BMI Calculator",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xff0a0e21),
+        ),
+        scaffoldBackgroundColor: const Color(0xff0a0e21),
+      ),
+      home: InputPage(),
+      //routes: {
+      //   '/result' : (context) => ResultsPage(interpretation: '', bmiResults: '', resultText: '',),
+      // },
+    );
+  }
+}
